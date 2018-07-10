7691ef62034411b32572ab0acc9e90a20f8350bd
Mikhail Svetov <svmk-tmsk@yandex.ru>
2018-07-10 19:40:48 +0700

    Added sitemap 0.3.0 documentation

---
+++ sitemap/0.3.0/implementors/core/iter/traits/trait.Extend.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["bitflags"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"struct\" href=\"bitflags/example_generated/struct.Flags.html\" title=\"struct bitflags::example_generated::Flags\">Flags</a>&gt; for <a class=\"struct\" href=\"bitflags/example_generated/struct.Flags.html\" title=\"struct bitflags::example_generated::Flags\">Flags</a>",synthetic:false,types:["bitflags::example_generated::Flags"]},];
+implementors["xml"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"type\" href=\"xml/namespace/type.UriMapping.html\" title=\"type xml::namespace::UriMapping\">UriMapping</a>&lt;'a&gt;&gt; for <a class=\"struct\" href=\"xml/namespace/struct.Namespace.html\" title=\"struct xml::namespace::Namespace\">Namespace</a>",synthetic:false,types:["xml::namespace::Namespace"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"type\" href=\"xml/namespace/type.UriMapping.html\" title=\"type xml::namespace::UriMapping\">UriMapping</a>&lt;'a&gt;&gt; for <a class=\"struct\" href=\"xml/namespace/struct.NamespaceStack.html\" title=\"struct xml::namespace::NamespaceStack\">NamespaceStack</a>",synthetic:false,types:["xml::namespace::NamespaceStack"]},{text:"impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html\" title=\"trait core::iter::traits::Extend\">Extend</a>&lt;<a class=\"type\" href=\"xml/namespace/type.UriMapping.html\" title=\"type xml::namespace::UriMapping\">UriMapping</a>&lt;'b&gt;&gt; for <a class=\"struct\" href=\"xml/namespace/struct.CheckedTarget.html\" title=\"struct xml::namespace::CheckedTarget\">CheckedTarget</a>&lt;'a&gt;",synthetic:false,types:["xml::namespace::CheckedTarget"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ sitemap/0.3.0/implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["chrono"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"chrono/naive/struct.TsSeconds.html\" title=\"struct chrono::naive::TsSeconds\">TsSeconds</a>",synthetic:false,types:["chrono::naive::datetime::TsSeconds"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
